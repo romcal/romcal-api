69eb1126b1a6f366662efe13fa3696fc257fbd7a
Etienne <etienne.magnier@gmail.com>
2020-02-24 19:47:42 +0100

    Update doc

--- docs/api_project.js
+++ docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-02-18T19:20:00.962Z",
+    "time": "2020-02-24T18:46:11.113Z",
     "url": "http://apidocjs.com",
     "version": "0.20.0"
   }
